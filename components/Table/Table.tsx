bc84df1f1eaeda195a23aac67f140f34796a76a1
Roman Skaletskyi <roman.skaletskyi@magicpowered.io>
2022-02-19 14:07:06 +0200

    table update

--- components/Table/Table.tsx
+++ components/Table/Table.tsx
@@ -3,6 +3,36 @@ import cx from 'classnames';
 
 import styles from './Table.module.scss';
 
+const TABLE_MOCK_DATA = [
+  {
+    id: '0',
+    date: '01.02.2022',
+    time: '12:00',
+    address: '0x4g6h...4dfb',
+    qd: '12,000,000',
+    usdt: '$9,999,000.34',
+    gain: '$9,999,000.34',
+  },
+  {
+    id: '1',
+    date: '01.02.2022',
+    time: '12:34',
+    address: '0x345g...bn67',
+    qd: '12,000,000',
+    usdt: '$1,000.00',
+    gain: '$9,999,000.34',
+  },
+  {
+    id: '2',
+    date: '01.02.2022',
+    time: '14:56',
+    address: '0x1svf...kio9',
+    qd: '12,000,000',
+    usdt: '$100.56',
+    gain: '$9,999,000.34',
+  },
+];
+
 export interface TableProps extends React.HTMLAttributes<HTMLTableElement> {
   className?: string;
 }
@@ -14,33 +44,24 @@ const Table: React.VFC<TableProps> = ({ className, ...other }) => {
         <tr>
           <th>Date</th>
           <th>Address</th>
-          <th align='right'>QD bought</th>
-          <th align='right'>USDT deposited</th>
-          <th align='right'>Gain</th>
+          <th className={styles.right}>QD bought</th>
+          <th className={styles.right}>USDT deposited</th>
+          <th className={styles.right}>Gain</th>
         </tr>
       </thead>
       <tbody className={styles.body}>
-        <tr>
-          <td>01.02.2022<time>12:00</time></td>
-          <td>0x4g6h...4dfb</td>
-          <td align='right'>12,000,000</td>
-          <td align='right'>$9,999,000.34</td>
-          <td align='right'>$9,999,000.34</td>
-        </tr>
-        <tr>
-          <td>01.02.2022<time>12:34</time></td>
-          <td>0x345g...bn67</td>
-          <td align='right'>12,000,000</td>
-          <td align='right'>$1,000.00</td>
-          <td align='right'>$9,999,000.34</td>
-        </tr>
-        <tr>
-          <td>01.02.2022<time>14:56</time></td>
-          <td>0x1svf...kio9</td>
-          <td align='right'>12,000,000</td>
-          <td align='right'>$100.56</td>
-          <td align='right'>$9,999,000.34</td>
-        </tr>
+        {TABLE_MOCK_DATA.map((tr) => (
+          <tr key={tr.id}>
+            <td>
+              {tr.date}
+              <time>{tr.time}</time>
+            </td>
+            <td>{tr.address}</td>
+            <td className={styles.right}>{tr.qd}</td>
+            <td className={styles.right}>{tr.usdt}</td>
+            <td className={styles.right}>{tr.gain}</td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
