ba504666ad9071d42fa94707d3e4fd1fa51ace13
Pedro Simão <pedrolimao@gmail.com>
2022-03-04 18:04:36 +0100

    Fixed Max button

--- components/Mint/Mint.tsx
+++ components/Mint/Mint.tsx
@@ -47,6 +47,8 @@ const Mint: React.VFC = () => {
       currentTimestamp,
     );
   };
+  
+  // console.log({ mintValue, usdtValue, totalSupplyCap, totalSupply });
 
   useDebounce(
     mintValue,
@@ -114,13 +116,13 @@ const Mint: React.VFC = () => {
       return;
     }
 
-    const costOfOneQd = await qdAmountToUsdtAmt('1');
-    const balance = await usdtContract.balanceOf(selectedAccount);
-    const newValue = totalSupplyCap < balance ? totalSupplyCap : balance;
+    const costOfOneQd = Number(formatUnits(await qdAmountToUsdtAmt('1'), 6));
+    const balance = Number(formatUnits(await usdtContract.balanceOf(selectedAccount), 6));
+    const newValue = Number(totalSupplyCap) < balance ? totalSupplyCap : (balance / costOfOneQd);
+    // console.log({ costOfOneQd, balance, newValue, totalSupplyCap });c
     
-
     setMintValue(
-      `${(+formatUnits(newValue, 6) / +formatUnits(costOfOneQd, 6)).toFixed()}`,
+      `${newValue}`,
     );
 
     if (inputRef) {
@@ -243,15 +245,15 @@ const Mint: React.VFC = () => {
   return (
     <form className={styles.root} onSubmit={handleSubmit}>
       <div>
-        {/*<div className={styles.availability}>*/}
-        {/*  <span className={styles.availabilityCurrent}>*/}
-        {/*    Minted {numberWithCommas(totalSupply)} QD*/}
-        {/*  </span>*/}
-        {/*  <span className={styles.availabilityDivideSign}>/</span>*/}
-        {/*  <span className={styles.availabilityMax}>*/}
-        {/*    {numberWithCommas(totalSupplyCap.toFixed())} QD mintable*/}
-        {/*  </span>*/}
-        {/*</div>*/}
+        <div className={styles.availability}>
+          {/*<span className={styles.availabilityCurrent}>*/}
+          {/*  Minted {numberWithCommas(totalSupply)} QD*/}
+          {/*</span>*/}
+          {/*<span className={styles.availabilityDivideSign}>/</span>*/}
+          <span className={styles.availabilityMax}>
+            {numberWithCommas(totalSupplyCap.toFixed())} QD mintable
+          </span>
+        </div>
         <div className={styles.inputContainer}>
           <input
             type="text"
